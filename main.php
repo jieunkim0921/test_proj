cd5b8cff671ddc206b82e32f4bafcf769527f598
jieun <aoghkwldms@naver.com>
2025-06-05 23:29:25 +0900

    250605/지은/메인 코드 수정

--- main.php
+++ main.php
@@ -3,9 +3,9 @@
 	include_once('./head_lib.php');
 	include_once('./dpbbs/newwin.inc.php');
 	include_once('./head.php');
-?>
+?>	
 
-<div class="test">sdfdsf</div>
+<div class="test">testtst</div>
 
 <style>
 	.test{width:50px; height:50px; background-image:url(http://dpunchlab.co.kr/adm/img/settings.svg?ic=check) no-repeat;}
