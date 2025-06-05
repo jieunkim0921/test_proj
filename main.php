f6a2f5eef4600e891184e6cbbcf335079434d85b
jieunkim0921 <155621765+jieunkim0921@users.noreply.github.com>
2025-06-05 23:16:30 +0900

    깃허브 테스트 메인 파일
    깃허브 테스트 메인 파일 업로드

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php
+	include_once('./common.php');
+	include_once('./head_lib.php');
+	include_once('./dpbbs/newwin.inc.php');
+	include_once('./head.php');
+?>
+
+<div class="test">sdfdsf</div>
+
+<style>
+	.test{width:50px; height:50px; background-image:url(http://dpunchlab.co.kr/adm/img/settings.svg?ic=check) no-repeat;}
+
+</style>	
+
+
+<?php include_once('tail.php');?>
+
