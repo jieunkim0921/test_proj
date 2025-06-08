aa80a049d0ef0d6219d82a106bed080ce67044f7
jieun <aoghkwldms@naver.com>
2025-06-09 00:14:53 +0900

    3번째 커밋

--- main.php
+++ main.php
@@ -5,9 +5,13 @@
 	include_once('./head.php');
 ?>	
 
-<div class="test">testtst</div>
-<div class="test">25.06.08 수정</div>
-<div class="test">25.06.08 두번째 수정</div>
+<style>
+.test03 {padding:50px; border:1px solid red; margin:20px 0;}
+</style>
+
+<div class="test test01">testtst</div>
+<div class="test test02">25.06.08 수정</div>
+<div class="test test03">25.06.08 두번째 수정</div>
 
 <style>
 	.test{width:50px; height:50px; background-image:url(http://dpunchlab.co.kr/adm/img/settings.svg?ic=check) no-repeat;}
